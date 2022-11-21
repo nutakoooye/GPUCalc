7797c931a66a720e317122aadf8ab31329240197
nutakoooye <andrkool151@gmail.com>
2022-11-21 11:07:38 +0300

    optimized using memory

--- with_numba/main.py
+++ with_numba/main.py
@@ -1,5 +1,6 @@
 import time
 
+start_time = time.time()
 import numpy as np
 import numba as nb
 
@@ -16,21 +17,20 @@ ts_g1.shape = (-1, ix_g1.shape[0])
 
 @nb.njit(["complex64[:,:](complex64[:], complex64[:,:])"], cache=True)
 def main_task(ix, ts):
-    convolution = np.empty_like(ts)
     ix = fft_1d(ix)
     for i in range(ts.shape[0]):
-        spectral = fft_1d(ts[i])
-        convolution[i] = ifft_1d(spectral * ix)
+        ts[i] = ifft_1d(fft_1d(ts[i]) * ix)
 
-    return convolution
+    return ts
 
+
+print("numba time - ", time.time() - start_time)
+input("PRESS ENTER TO START")
 
 start_time = time.time()
 main_task(ix_g1, ts_g1)
 print("with compiling - ", time.time() - start_time)
+
 start_time = time.time()
 main_task(ix_g1, ts_g1)
 print("after compiling - ", time.time() - start_time)
-# ts_g1 = convert_to_complex(ts_g1)
-# ts_g1.shape = (-1, ix_g1.shape[0])
-# print(fft_1d(ts_g1[0]))
