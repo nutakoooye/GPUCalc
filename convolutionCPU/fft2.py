8d0abb56360aa393fece6f9167291be7856ccce2
nutakoooye <andrkool151@gmail.com>
2023-01-04 18:26:41 +0300

    add example data

---
+++ convolutionCPU/fft2.py
@@ -0,0 +1,55 @@
+import numpy as np
+import numba as nb
+
+
+@nb.njit(["complex64[:](complex64[:])"], fastmath=True, cache=True)
+def DFT_1D(fx):
+    M = fx.shape[0]
+    fu = fx.copy()
+
+    for i in range(M):
+        u = i
+        sum = 0
+        for j in range(M):
+            x = j
+            tmp = fx[x]*np.exp(-2j*np.pi*x*u/M)
+            sum += tmp
+        # print(sum)
+        fu[u] = sum
+    # print(fu)
+
+    return fu
+
+@nb.njit(["complex64[:](complex64[:])"], fastmath=True, cache=True)
+def FFT_1D(fx):
+    M = fx.shape[0]
+    minDivideSize = 4
+
+    if M % 2 != 0:
+        raise ValueError("the input size must be 2^n")
+
+    if M <= minDivideSize:
+        return DFT_1D(fx)
+    else:
+        fx_even = FFT_1D(fx[::2])  # compute the even part
+        fx_odd = FFT_1D(fx[1::2])  # compute the odd part
+        W_ux_2k = np.exp(-2j * np.pi * np.arange(M) / M)
+
+        f_u = fx_even + fx_odd * W_ux_2k[:M//2]
+
+        f_u_plus_k = fx_even + fx_odd * W_ux_2k[M//2:]
+
+        fu = np.concatenate([f_u, f_u_plus_k])
+
+    return fu
+
+@nb.njit(["complex64[:](complex64[:])"], fastmath=True, cache=True)
+def inverseFFT_1D(fu):
+    fu_conjugate = np.conjugate(fu)
+
+    fx = FFT_1D(fu_conjugate)
+
+    fx = np.conjugate(fx)
+    fx = fx / fu.shape[0]
+
+    return fx
